1ab63c48d1b85bb2ce1a625ce0a7fdcaf29a3151
Stas Chernyi <itsmeprivet@mail.ru>
2025-09-01 22:53:15 +0300

    update README.md and small fixes funcs

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
 
-def filter_by_state(list_dicts: list, state: str = 'EXECUTED') -> list:
-    """Функция возвращает словари с ключом по умолчанию"""
-    return [list_dict for list_dict in list_dicts if list_dict["state"] == 'EXECUTED']
+def filter_by_state(transaction_list: list, state: str = "EXECUTED") -> list:
+    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
+    соответствует указанному значению."""
+    return [transaction for transaction in transaction_list if transaction["state"] == state]
 
 
-def sort_by_date(list_dicts: list, reverse: bool = True) -> list:
-    """Функция сортирует список по дате"""
-    return sorted(list_dicts, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
+def sort_by_date(transaction_list: list, ascending: bool = True) -> list:
+    """Функция возвращает новый список, отсортированный по дате"""
+    return sorted(transaction_list, key=lambda x: datetime.fromisoformat(x["date"]), reverse=ascending)

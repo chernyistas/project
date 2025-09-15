935279dfeb699cc80c8da1bef5d9348758a3df71
Stas Chernyi <itsmeprivet@mail.ru>
2025-09-15 19:19:07 +0300

    create funcs in generators

--- src/generators.py
+++ src/generators.py
@@ -1,9 +1,25 @@
-def filter_by_currency():
-    pass
+def filter_by_currency(transactions: list, currency: str) -> iter:
+    """Возвращает итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной"""
+    # Проверяем валюту операции
+    for transaction in transactions:
+        if transaction['operationAmount']['currency']['name'] == currency:
+            yield transaction
 
 
-def transaction_descriptions():
-    pass
+def transaction_descriptions(transactions: list) -> iter:
+    """Возвращает итератор, который поочередно выдает описание транзакции"""
+    for transaction in transactions:
+        yield transaction["description"]
 
-def card_number_generator():
-    pass
+
+def card_number_generator(start=1, end=9999999999999999):
+    """Генератор, который выдает номера банковских карт в формате
+XXXX XXXX XXXX XXXX, где X — цифра номера карты"""
+    # Форматируем число, добавляя ведущие нули и пробелы
+    current = start
+    while current <= end:
+        formatted_number = f"{current:016d}"
+        yield (f"{formatted_number[:4]} {formatted_number[4:8]} "
+               f"{formatted_number[8:12]} {formatted_number[12:]}")
+        current += 1
--- tests/conftest.py
+++ tests/conftest.py
@@ -20,3 +20,22 @@ def transactions() -> list:
         {"id": 4, "state": "FAILED", "date": "2023-10-04"},
         {"id": 5, "state": "EXECUTED", "date": "2023-10-05"},
     ]
+
+
+@pytest.fixture
+def transactions_() -> list:
+    return [{
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    }]

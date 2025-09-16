604becbb002b02c15e56b29786a8321529b3ef6e
Stas Chernyi <itsmeprivet@mail.ru>
2025-09-16 21:32:11 +0300

    create tests for funcs in generators

--- src/generators.py
+++ src/generators.py
@@ -1,25 +1,30 @@
-def filter_by_currency(transactions: list, currency: str) -> iter:
+from typing import Dict, Iterator, List
+
+
+def filter_by_currency(
+    transactions: List[Dict[str, Dict[str, Dict[str, str]]]], currency: str
+) -> Iterator[Dict[str, Dict[str, Dict[str, str]]]]:
     """Возвращает итератор, который поочередно выдает транзакции,
     где валюта операции соответствует заданной"""
     # Проверяем валюту операции
+
     for transaction in transactions:
-        if transaction['operationAmount']['currency']['name'] == currency:
+        if transaction["operationAmount"]["currency"]["name"] == currency:
             yield transaction
 
 
-def transaction_descriptions(transactions: list) -> iter:
+def transaction_descriptions(transactions: List[Dict[str, str]]) -> Iterator[str]:
     """Возвращает итератор, который поочередно выдает описание транзакции"""
     for transaction in transactions:
         yield transaction["description"]
 
 
-def card_number_generator(start=1, end=9999999999999999):
+def card_number_generator(start: int = 1, end: int = 9999999999999999) -> Iterator[str]:
     """Генератор, который выдает номера банковских карт в формате
-XXXX XXXX XXXX XXXX, где X — цифра номера карты"""
+    XXXX XXXX XXXX XXXX, где X — цифра номера карты"""
     # Форматируем число, добавляя ведущие нули и пробелы
     current = start
     while current <= end:
         formatted_number = f"{current:016d}"
-        yield (f"{formatted_number[:4]} {formatted_number[4:8]} "
-               f"{formatted_number[8:12]} {formatted_number[12:]}")
+        yield (f"{formatted_number[:4]} {formatted_number[4:8]} " f"{formatted_number[8:12]} {formatted_number[12:]}")
         current += 1

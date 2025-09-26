2d7f100be27cc1a9f28b5e1aaa528af8b358502e
Stas Chernyi <itsmeprivet@mail.ru>
2025-09-26 20:19:13 +0300

    create decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,44 @@
+from functools import wraps
+from typing import Any, Callable, Optional, ParamSpec, TypeVar
+
+T = TypeVar('T')  # Обобщённый тип для возвращаемого значения
+P = ParamSpec('P')  # Параметры функции, которую декорируем
+
+
+def log(filename: Optional[str] = None) -> Callable[[Callable[P, T]], Callable[P, T]]:
+    """Декоратор автоматически логирует начало и конец выполнения функции,
+    а также ее результат или возникшие ошибки."""
+
+    def decorator(func: Callable[P, T]) -> Callable[P, T]:
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+            # Определяем куда писать логи
+            if filename:
+                with open(filename, "a") as file:
+                    file.write(f"Start execution {func.__name__}\n")
+
+            else:
+                print(f"Start execution '{func.__name__}'")
+
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(f"{func.__name__} ok")
+                else:
+                    print(f"{func.__name__} ok")
+
+                return result
+
+            except Exception as e:
+
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(f"{func.__name__} error: {str(e)}. Inputs: {args}, {kwargs}")
+                else:
+                    print(f"{func.__name__} error: {str(e)}. Inputs: {args}, {kwargs}")
+                raise
+
+        return wrapper
+
+    return decorator
